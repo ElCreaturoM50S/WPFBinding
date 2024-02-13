fb3713911f7e4aa0ff191cbdc81cb53dbdd70ecf
bobu <113674920+ElCreaturoM50S@users.noreply.github.com>
2024-02-13 08:23:50 +0100

    Add project files.

---
+++ WPFBinding/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Entity.Migrations.Model;
+using System.Data.SQLite;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPFBinding
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private const string ConnectionString = "Data Source=MojaBaza.db;Version=3;";
+        public App()
+        {
+            CreateTable();
+        }
+
+        private void CreateTable()
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(ConnectionString))
+            {
+                connection.Open();
+
+                string query = "CREATE TABLE IF NOT EXISTS UserNames (name TEXT)";
+                using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
---
+++ WPFBinding/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Data.SQLite;
+using System.Data.SqlClient;
+
+namespace WPFBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const string ConnectionString = "Data Source=MojaBaza.db;Version=3;";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string idToSave = idBox.Text;
+            string nameToSave = nameBox.Text;
+            string ageToSave = ageBox.Text;
+
+            if(
+                !string.IsNullOrEmpty(ageToSave) && !string.IsNullOrWhiteSpace(ageToSave)
+                && !string.IsNullOrEmpty(idToSave) && !string.IsNullOrWhiteSpace(idToSave)
+                && !string.IsNullOrEmpty(nameToSave) && !string.IsNullOrWhiteSpace(nameToSave)
+               ) 
+            {
+                using(SQLiteConnection conn = new SQLiteConnection(ConnectionString)) 
+                { 
+                    conn.Open();
+
+                    string query = "INSERT INTO UserNames (id,name,age) VALUES (@saveId,@ageId,@nameId)";
+                    using (SQLiteCommand command = new SQLiteCommand(query, conn))
+                    {
+                        command.Parameters.AddWithValue("@saveId", idToSave);
+                        command.Parameters.AddWithValue("@ageId", ageToSave);
+                        command.Parameters.AddWithValue("@nameId", nameToSave);
+                        command.ExecuteNonQuery();
+                    }
+                }
+                MessageBox.Show($"Dane został zapisane ({idToSave} {ageToSave} {nameToSave}) została zapisana");
+            } else
+            {
+                MessageBox.Show("SDADSADSAD NIE DIZLAA");
+            }
+            try
+            {
+                string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                string filePath = System.IO.Path.Combine(desktopPath, "mojedane.txt");
+                File.WriteAllText(filePath, $"{idToSave} {nameToSave} {ageToSave}");
+                MessageBox.Show("Wszystko zapisane");
+            }   
+            catch (Exception ex)
+            {
+                MessageBox.Show("Błąd podczas zapisywanie " + ex.Message);
+            }
+        }
+    }
+}
